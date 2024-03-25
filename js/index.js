4270a2568f19ecd4d3a1875c1e969bc89df38751
Aliaksei Karzhou <alexius90e@gmail.com>
2024-03-26 00:26:35 +0300

    feat: added info-slider

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-import "./custom-select.js";
+import './custom-select.js';
+
+const swiper = new Swiper('.info-slider  .swiper', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
