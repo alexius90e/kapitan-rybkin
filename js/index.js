944f3d8ed2f3c487991994cbc8cd8b6ba974a0f0
Aliaksei Karzhou <alexius90e@gmail.com>
2024-03-27 19:51:05 +0300

    fix: fixed info-slider issues

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import './custom-select.js';
 
 const swiper = new Swiper('.info-slider  .swiper', {
   loop: true,
-
+  spaceBetween: 32,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
