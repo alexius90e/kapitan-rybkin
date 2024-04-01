d62e7b73df292d6b93fad733b678081c7ed80c63
Aliaksei Karzhou <alexius90e@gmail.com>
2024-04-01 12:08:44 +0300

    feat: added lang select

--- js/index.js
+++ js/index.js
@@ -8,3 +8,24 @@ const swiper = new Swiper('.info-slider  .swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const initialLanguage = localStorage.getItem('language');
+
+const langSelect = document.getElementById('language');
+const selectSelected = document.querySelector('#language ~ .select-selected');
+
+if (initialLanguage && langSelect && selectSelected) {
+  const changeEvent = new Event('change');
+  selectSelected.innerHTML = `<span>${initialLanguage.toLocaleUpperCase()}</span>`;
+  langSelect.dataset.value = initialLanguage.toLocaleUpperCase();
+  langSelect.value = initialLanguage.toLocaleUpperCase();
+  langSelect.dispatchEvent(changeEvent);
+  document.body.dataset.language = initialLanguage;
+} else {
+  localStorage.setItem('language', 'ru');
+}
+
+langSelect.addEventListener('change', (event) => {
+  document.body.dataset.language = event.target.value;
+  localStorage.setItem('language', event.target.value);
+});
